9d983fdcf2b674bfe926d94b69c80141aece4811
Nathan Daly <nhdaly@gmail.com>
2019-07-04 19:58:29 -0400

    Write all the functions to output proto

--- src/PProf.jl
+++ src/PProf.jl
@@ -2,6 +2,7 @@ module PProf
 
 include(joinpath("..", "lib", "perftools.jl"))
 
+using ProtoBuf
 using OrderedCollections
 
 """
@@ -22,56 +23,87 @@ end
 
 using Profile
 using Base.StackTraces: StackFrame
-with_value(f, x) = x !== nothing && f(x)
 
 # =================
 # NOTES
-# - Functions need a uid, we'll use the pointer for the method instance
 #
-
-prof = perftools.profiles.Profile(sample = [], location = [], _function = [],
-                                  mapping = [], string_table = [])
-
 function pprof(data::Array{UInt,1} = UInt[],litrace::Dict{UInt,Array{StackFrame,1}} = Dict{UInt,Array{StackFrame,1}}();
                          from_c=false)
+
+
+     string_table = OrderedDict{AbstractString, Int}()
+     enter!(string_table, "")  # NOTE: Google requires first entry to be ""
+
+     # Functions need a uid, we'll use the pointer for the method instance
+     funcs = Dict{UInt64, perftools.profiles.Function}()
+
+     prof = perftools.profiles.Profile(sample = [], location = [], _function = [],
+                                       mapping = [], string_table = [])
+
+
     if length(data) == 0
         (data, litrace) = Profile.retrieve()
     end
 
-    data, litrace = Profile.flatten(data, litrace)
+    #data, litrace = Profile.flatten(data, litrace)
 
     sample = perftools.profiles.Sample()
 
-    open("test.txt" "w", stdout) do formatter
-        lastwaszero = true
-        for d in data
-            if d == 0
-                # 
-                #if !lastwaszero
-                #    write(formatter, "\n")
-                #end
-                lastwaszero = true
-                continue
-            end
-            frame = litrace[d]
+    lastwaszero = true
+    for d in data
+        if d == 0
+            # End of sample
+            push!(prof.sample, sample)
+            sample = perftools.profiles.Sample()
+
+            #if !lastwaszero
+            #    write(formatter, "\n")
+            #end
+            lastwaszero = true
+            continue
+        end
+        frames = litrace[d]
+        for frame in frames
             if !frame.from_c || from_c
-                file = Base.find_source_file(string(frame.file))
-                func_line = frame.line
-                with_value(frame.linfo) do linfo
-                    func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
+                # Store the function in our functions dict
+                if !(frame.pointer in keys(funcs))
+                    funcProto = (funcs[frame.pointer] = perftools.profiles.Function())
+                    funcProto.id = frame.pointer
+                    funcProto.name = enter!(string_table, string(frame.func))
+                    if frame.linfo !== nothing
+                        # TODO
+                        # ... get full method name w/ types
+                    end
+                    funcProto.system_name = funcProto.name
+                    file = Base.find_source_file(string(frame.file))
+                    file_repr = file == nothing ? "nothing" : file
+                    funcProto.filename = enter!(string_table, file_repr)
+                    funcProto.start_line = frame.line
                 end
 
-                file_repr = file == nothing ? "nothing" : file
-                write(formatter, "$(file_repr)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
                 lastwaszero = false
             end
         end
     end
 
-    @info "Wrote profiling output to file://$(pwd())/statprof/index.html ."
+    # Build Profile
+    prof.string_table = collect(keys(string_table))
+    prof._function = collect(values(funcs))
+
+    # Write to
+    f = "out.proto"
+    open("out.proto", "w") do io
+        writeproto(io, prof)
+    end
+
+
+    @show prof
+    f
 end
 
 
+
+pprof()
 
 
 end # module
