9cf4ae4fc4ae8541ffc1c8f7bc4c0b051594688a
Nathan Daly <nhdaly@gmail.com>
2019-07-30 13:29:52 -0400

    Add BinaryProvider deps for go tool pprof

--- src/PProf.jl
+++ src/PProf.jl
@@ -6,6 +6,20 @@ using Profile
 using ProtoBuf
 using OrderedCollections
 
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    error("LibFoo not installed properly, run Pkg.build(\"LibFoo\"), restart Julia and try again")
+end
+include(depsjl_path)
+
+# Module initialization function
+function __init__()
+    # Always check your dependencies from `deps.jl`
+    check_deps()
+end
+
+
 include(joinpath("..", "lib", "perftools.jl"))
 
 import .perftools.profiles: ValueType, Sample, Function,
