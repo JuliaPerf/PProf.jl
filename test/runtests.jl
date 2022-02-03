826063692e1d5a30f751a04a4e767febb8803175
Nathan Daly <nhdaly@gmail.com>
2022-02-03 17:45:05 -0500

    Use `isdefined()` instead of testing `VERSION` for `Profile.Allocs`
    This lets PProf continue to work for custom builds of julia that have
    Allocs profiling available, such as if you backport the allocations
    profiler to an earlier version of julia

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ end
     include("flamegraphs.jl")
 end
 
-if VERSION >= v"1.8.0-DEV.1346"  # PR https://github.com/JuliaLang/julia/pull/42768
+@static if isdefined(Profile, :Allocs)  # PR https://github.com/JuliaLang/julia/pull/42768
 @testset "Allocs.jl" begin
     include("Allocs.jl")
 end
